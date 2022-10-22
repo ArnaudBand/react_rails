c7f05e24014796266e757411a98b553445cb604c
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-22 02:35:38 -0600

    Import Provider to fetch from the store

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,9 +4,14 @@ import "./controllers"
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/configureStore";
+import App from './App'
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
